3d1474d6e2d803f2a96d9d24d9eed3db36e51052
Roman <Isekeeper@gmail.com>
2023-03-18 18:10:31 +0300

    Database class based on file is done

---
+++ app/src/main/java/com/example/salaryzer/DataBase.kt
@@ -0,0 +1,32 @@
+package com.example.salaryzer
+
+import androidx.core.text.isDigitsOnly
+
+
+class DataBase (data: String) {
+
+    class Salary (salaryInfo: String, types: String) {
+        val date: Pair<Int, Int>
+
+        val salary: ArrayList<Pair<Float, String>> = ArrayList()
+
+        init{
+            date = Pair(salaryInfo.substring(0, 2).toInt(), salaryInfo.substring(3, 7).toInt())
+            val salaryDigits = salaryInfo.substring(8).split(" ")
+            val salaryTypes = types.substring(11).split(" ")
+            for(i in 0 until salaryDigits.size){
+                salary.add(Pair(salaryDigits[i].toFloat(),salaryTypes[i]))
+            }
+        }
+
+    }
+    private val salaryList: ArrayList<Salary> = ArrayList()
+
+    init{
+        val dataLines = data.lines()
+        for (i in 1..dataLines.size-1){
+            salaryList.add(Salary(dataLines[i], dataLines[0]))
+        }
+    }
+
+}
--- app/src/main/java/com/example/salaryzer/MainActivity.kt
+++ app/src/main/java/com/example/salaryzer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.salaryzer
 import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,12 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import java.io.BufferedReader
+import java.io.File
+import java.io.File.separator
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
 //import by.dzmitry_lakisau.month_year_picker_dialog.MonthYearPickerDialog
 import java.util.*
 import kotlin.collections.ArrayList
@@ -20,6 +27,7 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     private lateinit var textDatePicker: Button
     lateinit var pieChart: PieChart
+    lateinit var db: DataBase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +37,9 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         textDatePicker = findViewById(R.id.btnMonthAndYear)
         pieChart = findViewById(R.id.pie_chart)
 
-        setPieChart(ArrayList(),firstTime = true)
+        setDB()
+
+        setPieChart(ArrayList(), firstTime = true)
 
         //                                                      DATA PICKER
 
@@ -38,35 +48,41 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
         // Open date picker on click
         textDatePicker.setOnClickListener() {
-             DatePickerDialog(this,
-                 this,
-                 calendar.get(Calendar.YEAR),
-                 calendar.get(Calendar.MONTH),
-                 calendar.get(Calendar.DAY_OF_MONTH)).show()}
+            DatePickerDialog(
+                this,
+                this,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
-
-    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
-        val arrayPairs :ArrayList<Pair<Float, String>> = ArrayList()
-        arrayPairs.add(Pair(100f,"first"))
-        arrayPairs.add(Pair(200f,"second"))
-        setPieChart(arrayPairs ,centerText = "${getTextMonth(month + 1)} $year")
     }
 
-    private fun setPieChart(pieArray: ArrayList<Pair<Float, String>>, firstTime: Boolean = false, centerText: String = ""){
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        val arrayPairs: ArrayList<Pair<Float, String>> = ArrayList()
+        arrayPairs.add(Pair(100f, "first"))
+        arrayPairs.add(Pair(200f, "second"))
+        setPieChart(arrayPairs, centerText = "${getTextMonth(month + 1)} $year")
+    }
+
+    private fun setPieChart(
+        pieArray: ArrayList<Pair<Float, String>>,
+        firstTime: Boolean = false,
+        centerText: String = ""
+    ) {
         // Initialing data for pie chart
         val pieList: ArrayList<PieEntry> = ArrayList()
         val pieDataSet = PieDataSet(pieList, "Data for pie chart")
 
-        if(firstTime){
+        if (firstTime) {
             pieList.add(PieEntry(100f, ""))
 
             pieDataSet.setColors(Color.GREEN)
             pieDataSet.valueTextSize = 15f
             pieDataSet.valueTextColor = Color.TRANSPARENT
             pieChart.centerText = ""
-        }
-        else{
-            for(i in pieArray)
+        } else {
+            for (i in pieArray)
                 pieList.add(PieEntry(i.first, i.second))
             pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
             pieDataSet.valueTextSize = 15f
@@ -78,6 +94,48 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         pieChart.description.text = ""
         pieChart.animateY(500)
     }
+
+    fun setDB() {
+        val fileName = "Database.db"
+        val filePath = filesDir
+        val path = filePath.toString() + "/" + fileName
+        val file = File(path)
+        val data: String
+
+        if (!file.exists() && !file.isDirectory) {
+            createDB(filePath, fileName)
+        }
+
+        data = readFile(File(filePath, fileName))
+
+        db = DataBase(data)
+
+
+    }
+
+    fun readFile(file: File) :String{
+        val inputStream = FileInputStream(file)
+        val reader = InputStreamReader(inputStream)
+        val bufferedReader = BufferedReader(reader)
+        val data = bufferedReader.use{
+            it.readLines().joinToString(separator = "\n")
+        }
+
+        return data
+    }
+
+    fun createDB(filePath: File, fileName: String) {
+        val file = File(filePath, fileName)
+        FileOutputStream(file).use {
+            it.write(("Month Year Аванс Отпускные Больничный Зарплата Премиальные" +
+                    System.getProperty("line.separator") +
+                    "03 2023 300.03 400.03 500.03 600.83 700.03" +
+                    System.getProperty("line.separator") +
+                    "02 2023 300.02 400.02 500.02 600.02 700.02").toByteArray())
+        }
+    }
+
+
 
 }
 
